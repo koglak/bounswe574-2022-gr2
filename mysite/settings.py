5a5ec17199c52cb33f6f36de532f542ec6e6684d
Kardelen Oğlakcıoğlu Özen <koglakk@gmail.com>
2022-04-03 01:08:14 +0300

    db migrated to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'swe573',
+        'USER':  'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
