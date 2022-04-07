7f89d787173ec6079d87d08222e5412b9e20c3cb
Kardelen Oğlakcıoğlu Özen <koglakk@gmail.com>
2022-04-08 00:45:05 +0300

    profile block added at myspace page

--- userprofile/views.py
+++ userprofile/views.py
@@ -8,6 +8,5 @@ from .models import Course, Profile
 
 def user_profile(response):
     courses=Course.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    print(response.user)
-    
-    return render(response, "userprofile/profile.html", {'courses':courses})
+    user_profile=Profile.objects.get(user=response.user)
+    return render(response, "userprofile/profile.html", {'courses':courses, 'user_profile':user_profile})
