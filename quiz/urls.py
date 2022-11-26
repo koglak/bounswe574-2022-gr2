120d98514adbde32ab9efe0af4148635bba55d0d
turanlioglu <erdemturanlioglu1995@gmail.com>
2022-11-26 17:01:44 +0300

    url patterns add for respected views

--- quiz/urls.py
+++ quiz/urls.py
@@ -9,6 +9,16 @@ urlpatterns = [
     path('<str:title>/quiz_delete',views.quiz_delete,name='quiz_delete'),
     path('<str:title>/case_create/', views.case_create,name='case_create'),
     path('<str:title>/case_detail/', views.case_detail,name='case_detail'),
+    path('comment/', views.comment_new, name='comment'),
+    path('<int:pk>/delete-comment',views.delete_comment,name='delete_comment'),
+    path('like/<int:pk>', views.LikeView_comment, name='like_comment'),
+    path('like/comment', views.LikeViewList_comment, name='like_comment_list'),
+    path('dislike/<int:pk>', views.DislikeView_comment, name='dislike_comment'),
+    path('dislike/question', views.DislikeViewList_comment, name='dislike_comment_list'),
+    path('<int:pk>/delete-question',views.delete_comment,name='delete_comment'),
+    path('like-answer/<int:pk>', views.ReplyCommentLikeView, name='like_replycomment'),
+    path('dislike-answer/<int:pk>', views.ReplyCommentDislikeView, name='dislike_replycomment'),
+
     path('<str:title>/case_grade/', views.case_grade,name='case_grade'),
     path('<int:pk>/case_rate',views.case_rate,name='case_rate'),
 
